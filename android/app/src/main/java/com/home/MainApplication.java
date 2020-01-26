86105b4fe31b692076ec29ec1cb6a63ddc930bde
ChamodKanishka <chamodkanishka77@gmail.com>
2020-01-26 13:15:58 +0530

    Fixed camera open issue in android and works fine. permission add manually and change some of android files

--- android/app/src/main/java/com/home/MainApplication.java
+++ android/app/src/main/java/com/home/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.wix.RNCameraKit.RNCameraKitPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
             new LocationServicesDialogBoxPackage(),
           new MapsPackage(),
             new RNGestureHandlerPackage(),
-            new VectorIconsPackage()
+            new VectorIconsPackage(),
+            new RNCameraKitPackage()
       );
     }
 
