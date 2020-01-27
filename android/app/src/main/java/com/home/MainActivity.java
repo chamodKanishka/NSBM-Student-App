b2699f11b72c0cdc2e8b322511366b7eb0e12b95
ChamodKanishka <chamodkanishka77@gmail.com>
2020-01-27 11:18:12 +0530

    add dark mode 2 flies two the project for future implementation

--- android/app/src/main/java/com/home/MainActivity.java
+++ android/app/src/main/java/com/home/MainActivity.java
@@ -4,6 +4,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import android.content.Intent;
+import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,6 +14,12 @@ public class MainActivity extends ReactActivity {
      * This is used to schedule rendering of the component.
      */
     @Override
+     public void onConfigurationChanged(Configuration newConfig) {
+     super.onConfigurationChanged(newConfig);
+     Intent intent = new Intent("onConfigurationChanged");
+     intent.putExtra("newConfig", newConfig);
+     sendBroadcast(intent);
+     }
     protected String getMainComponentName() {
         return "home";
     }
