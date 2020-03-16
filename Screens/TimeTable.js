6b04615b3e992e7c7ba05cc3c69577b96b975717
ChamodKanishka <chamodkanishka77@gmail.com>
2020-03-16 14:23:00 +0530

    remove timetable header

--- Screens/TimeTable.js
+++ Screens/TimeTable.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View , Text, StyleSheet} from 'react-native';
+import { Button, View , Text} from 'react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import NsbmShuttles from './components/Timetables/NsbmShuttles';
 import OtherShuttles from './components/Timetables/OtherShuttles';
@@ -10,46 +10,9 @@ const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-            <Header style={styles.header}>
-                <Left>
-                    <Icon name="menu" onPress={() =>this.props.navigation.openDrawer()}/>
-                </Left>
-                <Right style={styles.right}>
-                <Text style={styles.text}>Timetables</Text>
-                </Right>
-            </Header>
-    <Tab.Navigator style={styles.tab}>
+    <Tab.Navigator>
       <Tab.Screen name="NSBM Shuttles" component={NsbmShuttles} />
       <Tab.Screen name="Others" component={OtherShuttles} />
     </Tab.Navigator>
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    height: null,
-    width: null,
-    //justifyContent:,
-    alignItems:'stretch',
-    ...StyleSheet.absoluteFillObject,
-
-  },
-  tab: {
-      ...StyleSheet.absoluteFillObject,
-      marginTop:50,
-    },
-    header:{
-      backgroundColor:"#2196f3"
-  },
-  right:{
-      fontSize:24,
-      color:"white"
-  }, 
-  text:{
-      fontSize:24, 
-      color:"white"
-  }
-});
