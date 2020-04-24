d00c60fa0b78019482e49169c33c4bc7a3d259ea
ChamodKanishka <chamodkanishka77@gmail.com>
2020-04-24 23:05:39 +0530

    add pdf reading option to lecture materials function page

--- Screens/LectureMaterials.js
+++ Screens/LectureMaterials.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import{View, Text, StyleSheet} from "react-native";
+import{View, Text, StyleSheet, Dimensions} from "react-native";
 import { Header, Left, Right, Icon} from 'native-base';
+
+import Pdf from 'react-native-pdf';
 
 
 class LectureMaterials extends Component{
 
 
     render(){
-        const resourceType = 'base64';
+        const source = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf',cache:true};
         return(
             <View style={styles.container}>
             <Header style={styles.header}>
@@ -18,6 +20,21 @@ class LectureMaterials extends Component{
                 <Text style={styles.right}>Lecture Materials</Text>
                 </Right>
             </Header>
+            <Pdf
+                    source={source}
+                    onLoadComplete={(numberOfPages,filePath)=>{
+                        console.log(`number of pages: ${numberOfPages}`);
+                    }}
+                    onPageChanged={(page,numberOfPages)=>{
+                        console.log(`current page: ${page}`);
+                    }}
+                    onError={(error)=>{
+                        console.log(error);
+                    }}
+                    onPressLink={(uri)=>{
+                        console.log(`Link presse: ${uri}`)
+                    }}
+                    style={styles.pdf}/>
             </View>
         );
     }
@@ -34,5 +51,10 @@ const styles = StyleSheet.create({
     right:{
         fontSize:24,
         color:"white"
+    },
+    pdf: {
+        flex:1,
+        width:Dimensions.get('window').width,
+        height:Dimensions.get('window').height,
     }
 });
